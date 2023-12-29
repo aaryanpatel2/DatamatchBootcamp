201c833dea914866dc22af7093860fff9a33d1c0
Aaryan <aaryanp802@gmail.com>
2023-12-28 22:54:45 -0600

    all files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import CardEditor from './CardEditor';
+import CardViewer from './CardViewer';
+import Homepage from './Homepage';
+import PageRegister from './PageRegister';
+import PageLogin from './PageLogin';
 
-function App() {
+import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { isLoaded } from 'react-redux-firebase';
+
+const App = props => {
+  if (!isLoaded(props.auth, props.profile)) {
+    return <div>Authentication loading...</div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/">
+        <Homepage />
+      </Route>
+      <Route exact path="/editor">
+        <CardEditor />
+      </Route>
+      <Route exact path="/viewer/:deckId">
+        <CardViewer />
+      </Route>
+      <Route exact path="/register">
+        <PageRegister />
+      </Route>
+      <Route exact path="/login">
+        <PageLogin />
+      </Route>
+      <Route>
+        <div>Page not found!</div>
+      </Route>
+    </Switch>
   );
-}
+};
 
-export default App;
+const mapStateToProps = state => {
+  return { auth: state.firebase.auth, profile: state.firebase.profile };
+};
+
+export default connect(mapStateToProps)(App);
